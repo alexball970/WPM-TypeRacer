029ad31928742be91636e25b885071d2dd19540d
alexball970 <83504124+alexball970@users.noreply.github.com>
2022-04-17 10:48:54 +0200

    new functionality if time but not working lul

--- src/cs111/WPMProgram.java
+++ src/cs111/WPMProgram.java
@@ -6,34 +6,86 @@ import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 public class WPMProgram {
+    public WPMProgram() { }
 
     public static void main(String[] args) throws Exception {
+        StringBuffer desiredMutableString = new StringBuffer("");
+        int amountOfTime;
+        int amountOfWords;
+        String TIME = "time";
+        String AMOUNT = "amount";
 
         System.out.println("Hi, welcome to type racer");
         TimeUnit.SECONDS.sleep(1);
-        System.out.println("How many words do you want?");
-        Scanner scannedAmount = new Scanner(System.in);
-        int amount = scannedAmount.nextInt();
+        System.out.println("-time- or -amount- ?");
+        Scanner scannedCategory = new Scanner(System.in);
+        String category = scannedCategory.nextLine();
 
-        /**
-         * COUNTDOWN
-         */
+        if (Objects.equals(TIME, category)) {
+            System.out.println("How much time do you want?");
+            Scanner scannedAmount = new Scanner(System.in);
+            amountOfTime = scannedAmount.nextInt();
+            sleep();
+            getStringForRace(50, desiredMutableString);
+            getWPMByTime(String.valueOf(desiredMutableString), amountOfTime);
+        } else if (Objects.equals(AMOUNT,category)) {
+            System.out.println("How many words do you want?");
+            Scanner scannedAmount = new Scanner(System.in);
+            amountOfWords = scannedAmount.nextInt();
+            sleep();
+            getStringForRace(amountOfWords, desiredMutableString);
+            getWPMByAmount(String.valueOf(desiredMutableString));
+        }
+    }
+
+    public static String getRandomLineFromTheFile() throws Exception {
+        File file = new File("/Users/alexandreballenghien/Documents/WPM-TypeRacer/src/words.txt");
+        final RandomAccessFile f = new RandomAccessFile(file, "r");
+        final long randomLocation = (long) (Math.random() * f.length());
+        f.seek(randomLocation);
+        f.readLine();
+        String randomLine = f.readLine();
+        f.close();
+        return randomLine;
+    }
+
+
+    public static void getStringForRace(int amount, StringBuffer desiredMutableString) throws Exception {
+        StringJoiner desiredStringJoiner = new StringJoiner(" ");
+        for (int i = 1; i <= amount; i++) {
+            String line = getRandomLineFromTheFile();
+            System.out.print(line + " ");
+            desiredStringJoiner.add(line);
+        }
+        desiredMutableString.append(desiredStringJoiner);
+        System.out.println();
+    }
+
+    public static void sleep() throws InterruptedException {
         System.out.println("3");
         TimeUnit.SECONDS.sleep(1);
         System.out.println("2");
         TimeUnit.SECONDS.sleep(1);
         System.out.println("1");
         TimeUnit.SECONDS.sleep(1);
+    }
 
-        StringJoiner desiredString = new StringJoiner(" ");
-        for (int i = 1; i <= amount; i++) {
-            String line = getRandomLineFromTheFile(
-                    "/Users/alexandreballenghien/Documents/WPM-TypeRacer/src/words.txt");
-            System.out.print(line + " ");
-            desiredString.add(line);
-        }
-        System.out.println();
+    public static void getWPMByTime(String desiredString, long time) throws InterruptedException {
+        double start = LocalTime.now().toNanoOfDay() / 1000000000.0;
+        double end = start + time;
+        
+        Scanner scan = new Scanner(System.in);
+        String typedWords = scan.nextLine();
 
+        int numChars = typedWords.length();
+
+        int wpm = (int) ((((double) numChars / 5) / (double) time) * 60);
+
+        System.out.println("Your speed is " + wpm + " wpm");
+
+    }
+
+    public static void getWPMByAmount(String desiredString) {
         Scanner scan = new Scanner(System.in);
         double start = LocalTime.now().toNanoOfDay();
 
@@ -47,39 +99,11 @@ public class WPMProgram {
 
         int wpm = (int) ( ( ( (double) numChars / 5 ) / seconds ) * 60 );
 
-        if (Objects.equals(typedWords, desiredString.toString())) {
+        if (Objects.equals(typedWords, desiredString)) {
             System.out.println("Your speed is " + wpm + " wpm");
         } else {
             System.out.println("Bro, you dog shit");
         }
     }
-
-    public static String getRandomLineFromTheFile(String filePathWithFileName) throws Exception {
-
-        File file = new File("/Users/alexandreballenghien/Documents/WPM-TypeRacer/src/words.txt");
-        final RandomAccessFile f = new RandomAccessFile(file, "r");
-        final long randomLocation = (long) (Math.random() * f.length());
-        f.seek(randomLocation);
-        f.readLine();
-        String randomLine = f.readLine();
-        f.close();
-        return randomLine;
-    }
-
-    /**
-    public static void htmlWriter(String string) {
-
-        //String html = "<div><h1> This is test HTML</h1><p> This is a test paragraph for" + "our html page</p></div>";
-
-        File f = new File("/Users/alexandreballenghien/Desktop");
-        try {
-            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(f));
-            bufferedWriter.write(string);
-            bufferedWriter.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    **/
 
 }
